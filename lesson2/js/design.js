e3c497cbd448157480e1af9a017ac50455419340
choromba <horombacollin@gmail.com>
2022-04-18 23:23:53 +0200

    commit changes

--- lesson2/js/design.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-
-let newUpdate = document.getElementById('footer-sect');
-newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
-newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
