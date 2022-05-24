7594fbd6a263643742754703cd7070eb8188264c
choromba <horombacollin@gmail.com>
2022-05-24 18:28:11 +0200

    new commit

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -12,6 +12,29 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;//
-//////////////////////////////////////////////////////
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
 
+
+
+
+
+
+
+
+let d = new Date();
+
+let newUpdate = document.getElementById('footer-sect');
+newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
